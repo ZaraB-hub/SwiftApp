880b72c31b8f3143efcd64900a568ddbdb90f1c2
Zara <zarabahtanovic@gmail.com>
2026-02-22 20:02:25 +0100

    Initial Commit

---
+++ SwiftApp/SwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppApp.swift
+//  SwiftApp
+//
+//  Created by Zlatan Bahtanović on 22. 2. 26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
